e5a433416fd57eb83b467bb628805c19f1f3f54e
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-03-23 13:26:25 -0600

    se agrego la variable var_dump

--- tipos_funciones.php
+++ tipos_funciones.php
@@ -8,3 +8,6 @@ echo "Hola Mundo";
 //2. print() imprime una cadena, pero a diferencia de echo, print devuelve un valor (1) y puede ser usado en expresiones.
 print "Hola Como estas?";
 
+//3. var_dump() muestra información detallada sobre una variable, incluyendo su tipo y valor.
+$variable = "Curso de Informatica 2026";
+
