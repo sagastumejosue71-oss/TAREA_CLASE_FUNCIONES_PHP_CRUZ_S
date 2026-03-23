32339f6084dd7f27b6c09c258fdca6c7b5cbfee1
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-03-23 13:25:33 -0600

    se agrego la variable print

--- tipos_funciones.php
+++ tipos_funciones.php
@@ -4,3 +4,7 @@
 
 //1. echo() Muestra una o mas cadenas de texto en la pantalla.
 echo "Hola Mundo";
+
+//2. print() imprime una cadena, pero a diferencia de echo, print devuelve un valor (1) y puede ser usado en expresiones.
+print "Hola Como estas?";
+
